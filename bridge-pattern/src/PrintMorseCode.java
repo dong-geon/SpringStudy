d0c5ea086af54245bd5c10bb4cfe9adb2d551267
dong-geon <wge3142@gmail.com>
2023-05-24 14:43:04 +0900

    Bridge-Pattern

---
+++ bridge-pattern/src/PrintMorseCode.java
@@ -0,0 +1,23 @@
+public class PrintMorseCode extends MorseCode{
+    //donggeon
+    public PrintMorseCode d(){
+        dash();dot();dot();space();
+        return this;
+    }
+    public PrintMorseCode o(){
+        dash();dash();dash();space();
+        return this;
+    }
+    public PrintMorseCode n(){
+        dash();dot();space();
+        return this;
+    }
+    public PrintMorseCode g(){
+        dash();dash();dot();space();
+        return this;
+    }
+    public PrintMorseCode e(){
+        dot();space();
+        return this;
+    }
+}
